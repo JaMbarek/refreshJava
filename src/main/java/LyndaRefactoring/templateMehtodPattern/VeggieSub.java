cf0cb8805819928b12642b74c6976e74546747e1
JawharMbarek <mbarek.jawhar@yahoo.de>
2019-04-28 14:12:20 +0200

    Refactoring: Umsetzung des Template Pattern

---
+++ src/main/java/LyndaRefactoring/templateMehtodPattern/VeggieSub.java
@@ -0,0 +1,18 @@
+package LyndaRefactoring.templateMehtodPattern;
+
+public class VeggieSub extends Sandwitch {
+
+    boolean customerWantsMeat() {
+        return false;
+    }
+
+    @Override
+    void addMeat() {
+
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Vinger and Oil Added");
+    }
+}
